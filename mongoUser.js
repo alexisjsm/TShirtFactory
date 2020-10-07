e002b611a3bb8b1ed3dae124250bdd348811e2de
Alexis Jesús <cv.alexisjesus@gmail.com>
2020-10-07 13:10:54 +0200

    refactor: mongoUser

--- mongoUser.js
+++ mongoUser.js
@@ -1,8 +1,22 @@
+
 db.createUser({
-  user: 'admin',
-  pwd: 'password',
-  roles: [{
-    role: "readWrite",
-    db: "tshirtFactoryDB"
+  user: "admin",
+  pwd: "password",
+  roles: [
+    {
+      role: "readWrite",
+      db: "tshirtFactoryDB"
+    }]
+})
+
+db = db.getSiblingDB("tshirtFactoryDB_test")
+
+db.createUser({
+  user: "admin",
+  pwd: "password",
+  roles: [
+    {
+      role: "readWrite",
+      db: "tshirtFactoryDB_test"
     }]
 })
